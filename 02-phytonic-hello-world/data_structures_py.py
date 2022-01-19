f6abad3fd1e2f716056c16e7aed6afbf6a032eef
unknown <cristianioancostoiu@gmail.com>
2022-01-19 21:33:59 +0200

    Curs02

---
+++ 02-phytonic-hello-world/data_structures_py.py
@@ -0,0 +1,26 @@
+lista=[8,2,-5,"string",2.7]
+#print(type(lista))
+#print(lista[3])
+#print(lista[5])# daca indexul nu esita apare eraore
+#print(lista[1:])#operator de slicing elem de la 1 pana la finalul, listei
+#print(lista[1:3])
+#print(lista[5:])#### afiseaza ceva, fara : eroare
+#print(lista[0:5:2])# elementele de la index de la 0 pana lafinalul  minus 1
+#print(lista[-1])# ultimul element din lista
+#print(lista[-1::-1])
+#lista.append("mere")
+#lista[5]="mere"
+#print(lista)
+#print(lista.index(-5))
+#lista.insert(3, "element nou")
+#lista.pop(2)
+#lista.clear()
+#lista.reverse()
+#lista.pop(3)
+#lista.sort()
+lista_stringuri=['8','2','21']
+print(ord('2'),'2')
+print(ord('1'), '1')
+print(ord('2'),'2')
+lista_stringuri.sort()
+print(lista_stringuri)
